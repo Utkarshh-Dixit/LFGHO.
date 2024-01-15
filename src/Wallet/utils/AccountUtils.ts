63decd87539fa135a9a2230e8ab0bbb494b8d77f
shashank-mishra14 <shashank.mishra.dev14@gmail.com>
2024-01-15 21:31:19 +0530

    changes

--- src/Wallet/utils/AccountUtils.ts
+++ src/Wallet/utils/AccountUtils.ts
@@ -1,17 +1,30 @@
-import {ethers} from "ethers";
+import { Wallet } from 'ethers';
+import { Account } from '../models/Account';
 
-const generateKeys =(seedPhrase?: string) => {
-    let wallet = ethers.Wallet;
-    if(seedPhrase){
-        wallet = ethers.Wallet.fromMnemonic(seedPhrase);
-    }else{
-        wallet=ethers.Wallet.createRandom();
-        seedPhrase=wallet.mnemonic;
-    }
+export function generateAccount(seedPhrase: string = "", index: number = 0): 
+{ account: Account, seedPhrase: string } {
+  let wallet: Wallet;
 
-    const privateKey = wallet.privateKey;
-    const address = wallet.address;
+  // If the seed phrase is not provided, generate a random mnemonic using a CSPRNG
+  if (seedPhrase === "") {
+    seedPhrase = Wallet.createRandom().mnemonic.phrase;
+  }
 
-    return {seedPhrase, address, privateKey}
+  // If the seed phrase does not contain spaces, it is likely a mnemonic
+  wallet = (seedPhrase.includes(" ")) ? Wallet.fromMnemonic(seedPhrase, `m/44'/60'/0'/0/${index}`) : 
+  new Wallet(seedPhrase);
+
+  const { address } = wallet;
+  const account = { address, privateKey: wallet.privateKey, balance: "0" };
+  
+  // If the seedphrase does not include spaces then it's actually a private key, so return a blank string.
+  return { account, seedPhrase: seedPhrase.includes(" ")? seedPhrase : "" };
 }
 
+export function shortenAddress(str: string, numChars: number=4) {
+  return `${str.substring(0, numChars)}...${str.substring(str.length - numChars)}`;
+}
+
+export function toFixedIfNecessary( value: string, decimalPlaces: number = 2 ){
+  return +parseFloat(value).toFixed( decimalPlaces );
+}
