6b332db0456dacb8e7e48786392b6930f1e3c47f
shashank-mishra14 <shashank.mishra.dev14@gmail.com>
2024-01-15 17:58:55 +0530

    changes

--- Contracts/MultiSig/MultiSig.sol
+++ Contracts/MultiSig/MultiSig.sol
@@ -1,79 +1,74 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.2;
 
-contract MultiSig {
-    address[] public owners;
-    uint256 public numConfirmationsReq;
+// Import necessary libraries and interfaces
+import "./Issuance.sol";
+import "./TransactionProccesing.sol";
+import "./InterestCalculation.sol";
+import "./PaymentSettlement.sol";
 
-    struct Transaction {
-        address to;
-        uint256 value;
-        bool executed;
+contract MultiSigCreditCardWallet {
+    address public owner;
+    uint256 public requiredSignatures;
+
+    // Address of the credit card issuance contract
+    Issuance public creditIssuance;
+
+    // Address of other credit-related contracts
+    TransactionProcessing public transactionProcessing;
+    InterestCalculation public interestCalculation;
+    PaymentSettlement public paymentSettlement;
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Not the owner");
+        _;
     }
 
-    mapping(uint256 => mapping(address => bool)) public isConfirmed;
-    Transaction[] public transactions;
-
-    event TransactionSubmitted(uint transactionId, address sender, address reciever,uint amount);
-    event TransactionConfirmed(uint transactionId);
-    event TransactionExecuted(uint transactionId);
-    constructor(address[] memory _owners, uint256 _numConfirmedReq) {
-        require(_owners.length > 1, "Owners must be greater than 1");
-        require(
-            _numConfirmedReq > 0 && _numConfirmedReq <= _owners.length,
-            "Invalid number of confirmations required"
-        );  
-        for (uint256 i = 0; i < _owners.length; i++) {
-            require(_owners[i] != address(0), "Invalid Owner");
-            owners.push(_owners[i]);
-        }
-
-        numConfirmationsReq = _numConfirmedReq;
+    modifier hasEnoughSignatures() {
+        require(msg.sender == owner || requiredSignatures == 0, "Not enough signatures");
+        _;
     }
 
-    function submitTransaction(address _to) public payable {
-        require(_to != address(0), "Invalid Address");
-        require(msg.value > 0, "Transfer amount must be greater than 0");
+    constructor(
+        uint256 _requiredSignatures,
+        address _creditIssuance,
+        address _transactionProcessing,
+        address _interestCalculation,
+        address _paymentSettlement
+    ) {
+        owner = msg.sender;
+        requiredSignatures = _requiredSignatures;
 
-        uint transactionId = transactions.length;
-        transactions.push(Transaction({
-            to: _to,
-            value: msg.value,
-            executed: false
-        }));
-        emit TransactionSubmitted(transactionId, msg.sender, _to, msg.value);
+        // Initialize credit-related contracts
+        creditIssuance = Issuance(_creditIssuance);
+        transactionProcessing = TransactionProcessing(_transactionProcessing);
+        interestCalculation = InterestCalculation(_interestCalculation);
+        paymentSettlement = PaymentSettlement(_paymentSettlement);
     }
 
-    function confirmTransaction(uint transactionId) public{
-        require(transactionId<transactions.length,"Invalid Transaction Id");
-        require(!isConfirmed[transactionId][msg.sender],"Transation is already confirmed");
-        isConfirmed[transactionId][msg.sender] = true;
-        emit TransactionConfirmed(transactionId);
-        if(isTransactionConfirmed(transactionId)){
-            executeTransaction(transactionId);
-        }
+    // Function to change the required signatures
+    function changeRequiredSignatures(uint256 _requiredSignatures) external onlyOwner {
+        requiredSignatures = _requiredSignatures;
     }
 
-
-    function executeTransaction(uint transactionId) public payable {
-        require(transactionId<transactions.length, "Invalid Transaction Id");
-        require(!transactions[transactionId].executed, "Transaction is already executed");
-        (bool success,)=transactions[transactionId].to.call{value:transactions[transactionId].value}("");
-        require(success,"Transaction Executed failed");
-        transactions[transactionId].executed=true;
-        emit TransactionExecuted(transactionId);
-        
+    // Functions related to credit card issuance
+    function issueCreditCard(address user, uint256 limit) external hasEnoughSignatures {
+       creditIssuance.issueCredit(user, limit);
     }
-    function isTransactionConfirmed(uint transactionId) internal view returns(bool){
-        require(transactionId<transactions.length, "Invalid Transaction Id");
-        uint confirmationCount;
 
-        for(uint i=0;i<owners.length;i++){
-            if(isConfirmed[transactionId][owners[i]]){
-                confirmationCount++;
-            }
-        } 
-        return confirmationCount>= numConfirmationsReq;
+    // Functions related to transaction processing
+    function processTransaction(address user, uint256 amount) external hasEnoughSignatures {
+        transactionProcessing.processTransaction(user, amount);
+    }
 
+    // Functions related to interest calculation
+    function calculateInterest(address user) external view returns (uint256) {
+        return interestCalculation.calculateInterest(user);
+    }
+
+    // Functions related to payment settlement
+    function settlePayment(address user, address merchant, uint256 amount) external hasEnoughSignatures {
+        paymentSettlement.settlePayment(user, merchant, amount);
     }
 }
+
