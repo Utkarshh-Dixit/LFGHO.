bf8b1dd127de57b2928c93695a8e9bf6cc7f32e4
shashank-mishra14 <shashank.mishra.dev14@gmail.com>
2024-01-21 16:27:29 +0530

    changes

--- Contracts/MultiSig/InterestCalculation.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.2;
-
-contract InterestCalculation {
-    function calculateInterest(address user) public view returns (uint256) {
-        // Implement interest calculation based on the credit card agreement
-    }
-}
--- Contracts/MultiSig/TransactionProccesing.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.2;
-
-contract TransactionProcessing {
-    function processTransaction(address user, uint256 amount) public {
-        // Verify available credit and process transaction
-        // Update user balances and other relevant details
-    }
-}
